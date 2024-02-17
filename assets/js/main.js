a55f8130c5948536056dbbd61b6338f803300162
stanner834 <stanner834@gmail.com>
2024-02-17 16:05:34 -0600

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@ window.addEventListener('scroll', scrollActive)
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2000,
+    duration: 5000,
     delay: 10,
 //     reset: true
 });
